b341093f86fe518f41ba433999838f88cd46ada6
TheVerseer <133086100+TheVerseer@users.noreply.github.com>
2024-01-28 15:57:50 -0600

    Update Colors.lua

--- API/Themes/Purple/Colors.lua
+++ API/Themes/Purple/Colors.lua
@@ -1,1 +1,9 @@
-
+	Primary = Color3.fromRGB(100, 30, 100),
+	PVariant = Color3.fromRGB(120, 50, 120),
+	Secondary = Color3.fromRGB(110, 40, 110),
+	
+	Success = Color3.fromRGB(99, 255, 82),
+	Failure = Color3.fromRGB(255, 66, 66),
+	
+	TextPrimary = Color3.fromRGB(255, 105, 255),
+	TextSecondary = Color3.fromRGB(200, 55, 200),
