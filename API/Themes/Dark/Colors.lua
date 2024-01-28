156cab566bd868fc3cefb9925dfb871e53d736ec
TheVerseer <133086100+TheVerseer@users.noreply.github.com>
2024-01-28 14:30:04 -0600

    Update and rename Colors to Colors.lua

---
+++ API/Themes/Dark/Colors.lua
@@ -0,0 +1,9 @@
+	Primary = Color3.fromRGB(30, 30, 30),
+	PVariant = Color3.fromRGB(50, 50, 50),
+	Secondary = Color3.fromRGB(40, 40, 40),
+	
+	Success = Color3.fromRGB(99, 255, 82),
+	Failure = Color3.fromRGB(255, 66, 66),
+	
+	TextPrimary = Color3.fromRGB(255, 255, 255),
+	TextSecondary = Color3.fromRGB(200, 200, 200),
