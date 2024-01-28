ac659ac34289757d7f50dbde0be279ce76b22dde
TheVerseer <133086100+TheVerseer@users.noreply.github.com>
2024-01-28 15:58:41 -0600

    Create Colors.lua

---
+++ API/Themes/Yellow/Colors.lua
@@ -0,0 +1,9 @@
+	Primary = Color3.fromRGB(80, 80, 30),
+	PVariant = Color3.fromRGB(100, 100, 50),
+	Secondary = Color3.fromRGB(90, 90, 40),
+	
+	Success = Color3.fromRGB(99, 255, 82),
+	Failure = Color3.fromRGB(255, 66, 66),
+	
+	TextPrimary = Color3.fromRGB(255, 255, 160),
+	TextSecondary = Color3.fromRGB(200, 200, 105),
